eb4711a1e9b16721686d8fa0d6c66c203a95e145
JlFerrete <joseluisferrete@gmail.com>
2020-07-14 09:05:34 +0200

    Adding entity Response and its relationship to the student and the question

--- src/main/java/com/formacionbdi/microservicios/app/respuestas/models/entity/Respuesta.java
+++ src/main/java/com/formacionbdi/microservicios/app/respuestas/models/entity/Respuesta.java
@@ -1,10 +1,16 @@
 package com.formacionbdi.microservicios.app.respuestas.models.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
+import com.formacionbdi.microservicios.commons.examenes.models.entity.Pregunta;
 
 @Entity
 @Table(name="respuestas")
@@ -15,6 +21,13 @@ public class Respuesta {
 	private Long id;
 	
 	private String texto;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Alumno alumno;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	private Pregunta pregunta;
+	
 
 	public Long getId() {
 		return id;
@@ -30,6 +43,22 @@ public class Respuesta {
 
 	public void setTexto(String texto) {
 		this.texto = texto;
+	}
+
+	public Alumno getAlumno() {
+		return alumno;
+	}
+
+	public void setAlumno(Alumno alumno) {
+		this.alumno = alumno;
+	}
+
+	public Pregunta getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(Pregunta pregunta) {
+		this.pregunta = pregunta;
 	}
 	
 	
