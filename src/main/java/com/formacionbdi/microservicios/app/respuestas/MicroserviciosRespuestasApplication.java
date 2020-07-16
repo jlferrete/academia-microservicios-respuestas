d082d8ea09ce3dc08af3c257fd50094625cf9d35
JlFerrete <joseluisferrete@gmail.com>
2020-07-16 07:25:09 +0200

    Testing the student's answers in Postman

--- src/main/java/com/formacionbdi/microservicios/app/respuestas/MicroserviciosRespuestasApplication.java
+++ src/main/java/com/formacionbdi/microservicios/app/respuestas/MicroserviciosRespuestasApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 @EntityScan({"com.formacionbdi.microservicios.app.respuestas.models.entity",
-	"com.formacionbdi.microservicios.commons.alumnos.models.entity",
 	"com.formacionbdi.microservicios.commons.examenes.models.entity"})
 public class MicroserviciosRespuestasApplication {
 
--- src/main/java/com/formacionbdi/microservicios/app/respuestas/controllers/RespuestaController.java
+++ src/main/java/com/formacionbdi/microservicios/app/respuestas/controllers/RespuestaController.java
@@ -1,5 +1,8 @@
 package com.formacionbdi.microservicios.app.respuestas.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +23,10 @@ public class RespuestaController {
 	
 	@PostMapping
 	public ResponseEntity<?> crear(@RequestBody Iterable<Respuesta> respuestas) {
-		
+		respuestas = ((List<Respuesta>) respuestas).stream().map(r -> {
+			r.setAlumnoId(r.getAlumno().getId());
+			return r;
+		}).collect(Collectors.toList());
 		Iterable<Respuesta> respuestasDb = service.saveAll(respuestas);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(respuestasDb);
