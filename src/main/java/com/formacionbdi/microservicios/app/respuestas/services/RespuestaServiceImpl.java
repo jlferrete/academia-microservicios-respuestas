954a6c24df42bfe0fdf795db7478d3091b7effdb
JlFerrete <joseluisferrete@gmail.com>
2020-07-16 12:39:12 +0200

    Getting questions answered per student and per question Ids distributed

--- src/main/java/com/formacionbdi/microservicios/app/respuestas/services/RespuestaServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/app/respuestas/services/RespuestaServiceImpl.java
@@ -2,6 +2,9 @@ package com.formacionbdi.microservicios.app.respuestas.services;
 
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.formacionbdi.microservicios.app.respuestas.clients.ExamenFeignClient;
 import com.formacionbdi.microservicios.app.respuestas.models.entity.Respuesta;
 import com.formacionbdi.microservicios.app.respuestas.models.repository.RespuestaRepository;
+import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
+import com.formacionbdi.microservicios.commons.examenes.models.entity.Pregunta;
 
 @Service
 public class RespuestaServiceImpl implements RespuestaService {
@@ -29,7 +34,20 @@ public class RespuestaServiceImpl implements RespuestaService {
 	@Override
 	@Transactional(readOnly = true)
 	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long AlumnoId, Long examenId) {
-		return null;
+		Examen examen = examenClient.obtenerExamenPorId(examenId);
+		List<Pregunta> preguntas = examen.getPreguntas();
+		List<Long> preguntaIds = preguntas.stream().map(p -> p.getId()).collect(Collectors.toList());
+		List<Respuesta> respuestas = (List<Respuesta>) repository.findRespuestaByAlumnoByPreguntaIds(AlumnoId, preguntaIds);
+		respuestas = respuestas.parallelStream().map(r -> {
+			preguntas.forEach(p -> {
+				if(p.getId() == r.getPreguntaId()) {
+					r.setPregunta(p);
+				}
+			});
+			return r;
+		}).collect(Collectors.toList());
+		
+		return respuestas;
 	}
 
 	@Override
